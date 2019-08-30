377c2d8b79520913c90f2ddbe1b41f7097c092df
Mauricio Codeguez <mauricio.codeguez@gmail.com>
2019-08-30 13:12:06 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci(int.Parse(Console.ReadLine()));
+        }
+
+        private static void Fibonacci(int n)
+        {
+            int x = 1;
+            int y = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int z = x + y;
+                Console.WriteLine(z);
+
+                y = x;
+                x = z;
+            }
+        }
+    }
+}
